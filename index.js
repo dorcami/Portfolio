297114af4f3d80d47b583ef0b3078831f7897496
dorcami <102697297+dorcami@users.noreply.github.com>
2022-05-09 21:27:34 +0300

    new ones

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function change(){
+    document.getElementById("left").textContent = "1st changed"
+    let right = document.getElementsByClassName("right")[0]
+    right.textContent = "2nd changed also"
+    console.log("6"+2)
+}
